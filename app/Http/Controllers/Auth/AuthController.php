26e03888b9597530e523b8a3460a6c8c558e9221
kakashi <nk29time@gmail.com>
2021-11-18 12:52:14 +0900

    lock

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -5,6 +5,7 @@ use App\Http\Requests\LoginFormRequest;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AuthController extends Controller
 {
@@ -22,13 +23,26 @@ class AuthController extends Controller
     public function login(LoginFormRequest $request)
     {
         $credentials = $request->only('email','password');
-
-        if(Auth::attempt($credentials)){
-            $request->session()->regenerate();
-
-            return redirect()->route('home')->with('success','ログイン成功しました！');
+        $user = User::where('email','=',$credentials['email'])->first();
+        if(!is_null($user)){
+            if($user->locked_flg === 1){
+                return back()->withErrors(['danger' => 'アカウントがロックされています。']);
+            }
+            if(Auth::attempt($credentials)){
+                $request->session()->regenerate();
+                if($user->error_count > 0){
+                     $user->error_count = 0;
+                     $user->save();
+                }
+                return redirect()->route('home')->with('success','ログイン成功しました！');
+                $user->error_count += 1;
+                if($user->error_count === 6){
+                    $user->locked_flg = 1;
+                    return back()->withErrors(['danger' => 'アカウントがロックされました。']);
+                }
+                $user->save();
+            }
         }
-
         return back()->withErrors(['danger' => 'メールアドレスまたはパスワードが違います。']);
     }
 
